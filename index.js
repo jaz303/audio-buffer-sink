8b9d9cff007bc188aa18ec9344defd95db51d1e4
Jason Frame <jason@onehackoranother.com>
2014-03-25 16:14:59 +0000

    tabs => spaces

--- index.js
+++ index.js
@@ -1,105 +1,105 @@
 module.exports = function(audioContext, opts) {
 
-	opts = opts || {};
+    opts = opts || {};
 
-	// configuration
-	var bufferSize 		= opts.bufferSize || 4096,
-		channelCount	= opts.channelCount || 2,
-		sampleRate 		= opts.sampleRate || null,
-		duration 		= opts.duration || null,
-		length 			= opts.length || null;
+    // configuration
+    var bufferSize      = opts.bufferSize || 4096,
+        channelCount    = opts.channelCount || 2,
+        sampleRate      = opts.sampleRate || null,
+        duration        = opts.duration || null,
+        length          = opts.length || null;
 
-	// output state
-	var outputLength	= null,
-		outputBuffer 	= null,
-		offset 			= 0;
+    // output state
+    var outputLength    = null,
+        outputBuffer    = null,
+        offset          = 0;
 
-	function createOutputBuffer() {
+    function createOutputBuffer() {
 
-		if (!sampleRate) {
-			throw new Error("can't create output buffer - sample rate is unknown");
-		}
+        if (!sampleRate) {
+            throw new Error("can't create output buffer - sample rate is unknown");
+        }
 
-		if (duration) {
-			outputLength = Math.floor(sampleRate * duration);
-			outputBuffer = audioContext.createBuffer(channelCount, outputLength, sampleRate);
-			return;
-		}
+        if (duration) {
+            outputLength = Math.floor(sampleRate * duration);
+            outputBuffer = audioContext.createBuffer(channelCount, outputLength, sampleRate);
+            return;
+        }
 
-		if (length) {
-			outputLength = length;
-			outputBuffer = audioContext.createBuffer(channelCount, outputLength, sampleRate);
-			return;
-		}
+        if (length) {
+            outputLength = length;
+            outputBuffer = audioContext.createBuffer(channelCount, outputLength, sampleRate);
+            return;
+        }
 
-		throw new Error("can't create output buffer - either duration or length is required");
+        throw new Error("can't create output buffer - either duration or length is required");
 
-	}
+    }
 
-	if (sampleRate) {
-		createOutputBuffer();
-	}
+    if (sampleRate) {
+        createOutputBuffer();
+    }
 
-	var processor = audioContext.createScriptProcessor(bufferSize, channelCount, channelCount);
+    var processor = audioContext.createScriptProcessor(bufferSize, channelCount, channelCount);
 
-	// workaround for bug wherein ScriptProcessorNode does not operate when
-	// its output is disconnected.
-	var nullGain = audioContext.createGainNode();
-	nullGain.gain.value = 0;
-	processor.connect(nullGain);
-	nullGain.connect(audioContext.destination);
+    // workaround for bug wherein ScriptProcessorNode does not operate when
+    // its output is disconnected.
+    var nullGain = audioContext.createGainNode();
+    nullGain.gain.value = 0;
+    processor.connect(nullGain);
+    nullGain.connect(audioContext.destination);
 
-	processor.onaudioprocess = function(evt) {
+    processor.onaudioprocess = function(evt) {
 
-		if (offset >= outputLength) {
-			return;
-		}
+        if (offset >= outputLength) {
+            return;
+        }
 
-		var inputBuffer = evt.inputBuffer,
-			inputLength = inputBuffer.length;
+        var inputBuffer = evt.inputBuffer,
+            inputLength = inputBuffer.length;
 
-		if (outputBuffer === null) {
-			if (sampleRate === null) {
-				sampleRate = inputBuffer.sampleRate;
-				createOutputBuffer();
-			}
-		}
+        if (outputBuffer === null) {
+            if (sampleRate === null) {
+                sampleRate = inputBuffer.sampleRate;
+                createOutputBuffer();
+            }
+        }
 
-		var samplesRemaining = outputLength - offset;
+        var samplesRemaining = outputLength - offset;
 
-		if (inputLength > samplesRemaining) {
-			for (var i = 0; i < channelCount; ++i) {
-				outputBuffer.getChannelData(i).set(
-					inputBuffer.getChannelData(i).subarray(0, samplesRemaining),
-					offset
-				);
-			}
-			offset = outputLength;
-		} else {
-			for (var i = 0; i < channelCount; ++i) {
-				outputBuffer.getChannelData(i).set(
-					inputBuffer.getChannelData(i),
-					offset
-				);
-			}
-			offset += inputLength;
-		}
+        if (inputLength > samplesRemaining) {
+            for (var i = 0; i < channelCount; ++i) {
+                outputBuffer.getChannelData(i).set(
+                    inputBuffer.getChannelData(i).subarray(0, samplesRemaining),
+                    offset
+                );
+            }
+            offset = outputLength;
+        } else {
+            for (var i = 0; i < channelCount; ++i) {
+                outputBuffer.getChannelData(i).set(
+                    inputBuffer.getChannelData(i),
+                    offset
+                );
+            }
+            offset += inputLength;
+        }
 
-	}
+    }
 
-	processor.getOutputBuffer = function() {
-		return outputBuffer;
-	}
+    processor.getOutputBuffer = function() {
+        return outputBuffer;
+    }
 
-	processor.reset = function() {
-		offset = 0;
-		if (sampleRate) {
-			createOutputBuffer();
-		} else {
-			outputBuffer = null;
-		}
-	}
+    processor.reset = function() {
+        offset = 0;
+        if (sampleRate) {
+            createOutputBuffer();
+        } else {
+            outputBuffer = null;
+        }
+    }
 
-	return processor;
+    return processor;
 
 }
